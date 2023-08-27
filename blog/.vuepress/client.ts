c17aa3d35d33c7a03d1faa681d0e39d910737392
wilberC <wilber.carrascal@gmail.com>
2023-08-27 04:00:08 -0500

    remove-unused-icons

--- blog/.vuepress/client.ts
+++ blog/.vuepress/client.ts
@@ -1,41 +1,17 @@
-import {defineClientConfig} from "@vuepress/client";
-import {addIcons} from "oh-vue-icons";
+import { defineClientConfig } from "@vuepress/client";
+import { addIcons } from "oh-vue-icons";
 import {
-    FaTag,
-    FaFortAwesome,
-
-    CoExternalLink,
-    FaUser,
-    // OiRocket,
-    // RiLinkM,
-    // RiGithubFill,
-    // RiLinkedinBoxFill,
-    // RiFacebookBoxFill,
-    // RiTwitterFill,
-    // RiZhihuLine,
-    // HiMail,
-    // FaPaw,
-    // AiCv,
-    // AiGoogleScholarSquare
+  FaTag,
+  FaFortAwesome,
+  CoExternalLink,
+  FaUser,
 } from "oh-vue-icons/icons";
 
 addIcons(
-    FaTag,
-    FaFortAwesome,
-
-    CoExternalLink,
-FaUser,
-    // OiRocket,
-    // RiLinkM,
-    // RiGithubFill,
-    // RiLinkedinBoxFill,
-    // RiFacebookBoxFill,
-    // RiTwitterFill,
-    // RiZhihuLine,
-    // HiMail,
-    // FaPaw,
-    // AiCv,
-    // AiGoogleScholarSquare
+  FaTag,
+  FaFortAwesome,
+  CoExternalLink,
+  FaUser,
 );
 
 export default defineClientConfig({});
